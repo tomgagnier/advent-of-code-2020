e13a5908d49ae548a917850732618491a045aeb6
Tom Gagnier (tgagnier) <tgagnier@cisco.com>
2020-12-04 13:50:47 -0500

    Day 4 submission

---
+++ dec04/passport-validator.rb
@@ -0,0 +1,38 @@
+require 'ostruct'
+
+class Passport < OpenStruct
+  def required_fields?
+    ([:ecl, :pid, :eyr, :hcl, :byr, :iyr, :hgt] - to_h.keys).empty?
+  end
+
+  def valid_year?(year_key, min, max)
+    itself[year_key] =~ /^\d+$/ && itself[year_key].to_i.between?(min, max)
+  end
+
+  def valid_height?
+    match = /^(?<height>\d+)(?<unit>cm|in)$/.match(itself[:hgt])
+    return false unless match
+    height = match[:height].to_i
+    (match[:unit] == 'cm') ? height.between?(150, 193) : height.between?(59, 76)
+  end
+
+  def valid?
+    required_fields? &&
+      valid_year?(:byr, 1920, 2002) &&
+      valid_year?(:iyr, 2010, 2020) &&
+      valid_year?(:eyr, 2020, 2030) &&
+      valid_height? &&
+      itself[:hcl] =~ /^#[0-9a-f]{6}$/ &&
+      itself[:ecl] =~ /^(amb|blu|brn|gry|grn|hzl|oth)$/ &&
+      itself[:pid] =~ /^\d{9}$/
+  end
+end
+
+passports = File.readlines('input.txt')
+                .map { |l| l.chomp.split(/ +/).map { |kv| kv.split(/ *: */) }.to_h }
+                .reduce([{}]) { |a, h| a[-1] = a[-1].merge(h); a << {} if h.empty?; a }
+                .map { |h| Passport.new(h) }
+
+puts "part 0: #{passports.count}"
+puts "part 1: #{passports.filter { |p| p.required_fields? }.count}"
+puts "part 2: #{passports.filter { |p| p.valid? }.count}"
