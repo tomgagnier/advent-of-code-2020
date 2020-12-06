86015ed90d6c1771806e7418afee68088ca470e3
Tom Gagnier (tgagnier) <tgagnier@cisco.com>
2020-12-06 11:40:34 -0500

    Dec 6 submission

---
+++ dec06/custom-customs.rb
@@ -0,0 +1,16 @@
+##!/usr/bin/env ruby
+
+groups = File.readlines('input.txt')
+             .map { |line| line.chomp }
+             .reduce([[]]) { |group, line| line.empty? ? group << [] : group[-1] << line; group }
+
+p groups.map { |forms| forms.join.each_char.sort.uniq }
+        .map { |unique_responses| unique_responses.size }
+        .reduce(:+)
+
+def to_char_freq(forms)
+  forms.reduce(Hash.new(0)) { |freq, form| form.each_char { |c| freq[c] += 1; }; freq }
+end
+
+p groups.map { |forms| to_char_freq(forms).filter { |_, freq| freq == forms.size }.count }
+        .reduce(:+)
