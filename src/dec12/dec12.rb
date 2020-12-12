8795783e3bbd4f52dd5d2fc2c78b6301e77592f9
Tom Gagnier (tgagnier) <tgagnier@cisco.com>
2020-12-12 01:58:57 -0500

    Dec 12 improvements

--- src/dec12/dec12.rb
+++ src/dec12/dec12.rb
@@ -6,39 +6,34 @@ def instructions(file)
       .map { |m| [m[1].to_sym, m[2].to_i] }
 end
 
-def manhattan(vectors)
-  vectors
-    .reduce([0, 0]) { |position, vector| [position[0] + vector[0], position[1] + vector[1]] }
-    .map { |v| v.abs }.reduce(:+)
+def manhattan(position)
+  position.map { |v| v.abs }.reduce(:+).truncate
 end
 
-def part1(instructions)
-  direction = 0
-  vectors = instructions
-              .map { |instruction, unit|
-                case instruction
-                when :E
-                  [unit, 0]
-                when :W
-                  [-unit, 0]
-                when :N
-                  [0, unit]
-                when :S
-                  [0, -unit]
-                when :L
-                  direction += unit
-                  [0, 0]
-                when :R
-                  direction -= unit
-                  [0, 0]
-                when :F
-                  radians = direction * Math::PI / 180
-                  [Math.cos(radians) * unit, Math.sin(radians) * unit]
-                else
-                  raise "unknown instruction #{instruction}"
-                end
-              }
-  manhattan(vectors);
+def part1(instructions, direction = 0, position = [0, 0])
+  return manhattan(position) if instructions.empty?
+  instruction, unit = instructions.shift
+  case instruction
+  when :E
+    position[0] += unit
+  when :W
+    position[0] -= unit
+  when :N
+    position[1] += unit
+  when :S
+    position[1] -= unit
+  when :L
+    direction += unit
+  when :R
+    direction -= unit
+  when :F
+    radians = direction * Math::PI / 180
+    position[0] += Math.cos(radians) * unit
+    position[1] += Math.sin(radians) * unit
+  else
+    raise "unknown instruction #{instruction}"
+  end
+  part1(instructions, direction, position)
 end
 
 def rotate(degrees, point)
@@ -46,41 +41,36 @@ def rotate(degrees, point)
   cos = Math.cos(radians)
   sin = Math.sin(radians)
   x, y = point
-  [ x * cos - y * sin, y * cos + x * sin]
+  [x * cos - y * sin, y * cos + x * sin]
 end
 
-def part2(instructions)
-  waypoint = [10, 1]
-  position = [0, 0]
-  instructions
-    .each { |instruction, unit|
-      case instruction
-      when :E
-        waypoint = [waypoint[0] + unit, waypoint[1]]
-      when :W
-        waypoint = [waypoint[0] - unit, waypoint[1]]
-      when :N
-        waypoint = [waypoint[0], waypoint[1] + unit]
-      when :S
-        waypoint = [waypoint[0], waypoint[1] - unit]
-      when :L
-        waypoint = rotate(unit, waypoint)
-      when :R
-        waypoint = rotate(-unit, waypoint)
-      when :F
-        position = [position[0] + unit * waypoint[0], position[1] + unit * waypoint[1]]
-      else
-        raise "unknown instruction #{instruction}"
-      end
-    }
-  manhattan([position])
+def part2(instructions, waypoint = [10, 1], position = [0, 0])
+  return manhattan(position) if instructions.empty?
+  instruction, unit = instructions.shift
+  case instruction
+  when :E
+    waypoint = [waypoint[0] + unit, waypoint[1]]
+  when :W
+    waypoint = [waypoint[0] - unit, waypoint[1]]
+  when :N
+    waypoint = [waypoint[0], waypoint[1] + unit]
+  when :S
+    waypoint = [waypoint[0], waypoint[1] - unit]
+  when :L
+    waypoint = rotate(unit, waypoint)
+  when :R
+    waypoint = rotate(-unit, waypoint)
+  when :F
+    position = [position[0] + unit * waypoint[0], position[1] + unit * waypoint[1]]
+  else
+    raise "unknown instruction #{instruction}"
+  end
+  part2(instructions, waypoint, position)
 end
 
 def evaluate(file)
-  instructions = instructions(file)
-
-  p part1(instructions)
-  p part2(instructions)
+  p part1(instructions(file))
+  p part2(instructions(file))
 end
 
 evaluate('test-input.txt')
