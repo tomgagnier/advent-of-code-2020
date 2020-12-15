d64331e23ce30ec09c8a0483430e84de25446d06
Tom Gagnier (tgagnier) <tgagnier@cisco.com>
2020-12-15 12:53:10 -0500

    Play ruby golf on dec15

--- src/dec15/dec-15.rb
+++ src/dec15/dec-15.rb
@@ -1,16 +1,15 @@
 def game(seq, limit)
-  hash = Hash.new(0)
-  seq[0..-2].each_with_index { |v, i| hash[v] = i + 1 }
-  position = seq.size - 1
-  value = seq[-1]
-  until position == limit - 1
+  position = seq.size
+  value = seq.pop
+  positions_by_value = seq.each_with_index.to_h { |v, i| [v, i + 1] }
+  until position == limit
+    previous_position = positions_by_value.has_key?(value) ? positions_by_value[value] : position
+    positions_by_value[value] = position
+    value = position - previous_position
     position += 1
-    previous_position = hash[value]
-    hash[value] = position
-    value = previous_position == 0 ? 0 : position - previous_position
   end
   value
 end
 
-p game([16,12,1,0,15,7,11], 2020)
+p game([16, 12, 1, 0, 15, 7, 11], 2020)
 p game([16,12,1,0,15,7,11], 30000000)
