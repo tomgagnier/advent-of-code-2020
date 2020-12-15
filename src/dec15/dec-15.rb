e8b5768ee2ed0bc0705431d200d760cf28d2a343
Tom Gagnier (tgagnier) <tgagnier@cisco.com>
2020-12-15 12:58:25 -0500

    Use named arguments

--- src/dec15/dec-15.rb
+++ src/dec15/dec-15.rb
@@ -1,15 +1,15 @@
-def game(seq, limit)
-  position = seq.size
-  value = seq.pop
-  positions_by_value = seq.each_with_index.to_h { |v, i| [v, i + 1] }
+def game(sequence:, limit:)
+  position = sequence.size
+  value = sequence.pop
+  last_positions = sequence.each_with_index.to_h { |v, i| [v, i + 1] }
   until position == limit
-    previous_position = positions_by_value.has_key?(value) ? positions_by_value[value] : position
-    positions_by_value[value] = position
-    value = position - previous_position
+    last_position = last_positions.has_key?(value) ? last_positions[value] : position
+    last_positions[value] = position
+    value = position - last_position
     position += 1
   end
   value
 end
 
-p game([16, 12, 1, 0, 15, 7, 11], 2020)
-p game([16,12,1,0,15,7,11], 30000000)
+p game(sequence: [16, 12, 1, 0, 15, 7, 11], limit: 2020)
+p game(sequence: [16, 12, 1, 0, 15, 7, 11], limit: 30000000)
