cc4e97b02bd7449ca14dfe724bacd7287082dc9f
Tom Gagnier (tgagnier) <tgagnier@cisco.com>
2020-12-17 20:32:03 -0500

    Dec 17 Generalize to N-Dimensions

---
+++ src/dec17/conway.rb
@@ -0,0 +1,53 @@
+require_relative '../aoc'
+
+# @param point the center of the neighborhood
+# @return the neighborhood of the point (any dimension), including the point itself
+def neighborhood_of(point)
+  dimension = point.size
+  neighborhood_range = 0...3 ** dimension
+  neighborhood_range
+    .map { |i|
+      (0...dimension).reduce([i]) { |a, i|
+        quotient = a[-1] / 3
+        a[-1] = a[-1] % 3 + point[i] - 1
+        a << quotient unless a.size == dimension
+        a
+      }
+    }
+end
+
+# @param active - a Set of active points in a Conway game
+# @param rule - activate the point given the active status and number of active neighbors
+# @return the set of next Conway generation of active points
+def conway(active:, rule:)
+  active
+    .reduce(Set[]) { |candidates, p| candidates + neighborhood_of(p) }
+    .reduce(Set[]) { |next_generation, candidate|
+      active_neighbors = neighborhood_of(candidate)
+        .filter { |neighbor| active.include?(neighbor) } - [candidate]
+      next_generation << candidate if rule.call(active.include?(candidate), active_neighbors.size)
+      next_generation }
+end
+
+# @return a set of active Conway cells read from the file_path
+def read_active(file_path:, dimensions: 0)
+  read_lines(file_path)
+    .each_with_index
+    .flat_map { |line, y|
+      line.each_char.each_with_index
+          .filter { |c, _| c == '#' }
+          .map { |_, x| [x, y] + Array.new(dimensions - 2, 0) } }
+    .to_set
+end
+
+rule = lambda { |is_active, count| is_active && count.between?(2, 3) || !is_active && count == 3 }
+
+%w(test-input.txt input.txt).each do |file_path|
+  puts file_path
+  (3..4).each do |dimension|
+    active = read_active(file_path: file_path, dimensions: dimension)
+    6.times { active = conway(rule: rule, active: active) }
+    puts "  dimensions: #{dimension}, active count: #{active.count}"
+  end
+end
+
